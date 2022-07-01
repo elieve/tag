57b7a80dd6fd58c42454be4da26603cb33736c76
elieve <bayustiadi@ymail.com>
2022-07-01 21:09:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "16589591"))
-    API_HASH = os.environ.get("API_HASH", "396d3b2b0f1cd575ab6a34fe65afd7cc")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5430925446:AAHKsxgxdetQSfzsfuFFhd6cGkXfOoPahAw")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOI0Bu13GLMU7zz5UqZv1mNf7mC2-Ri-q7VfDrX3ZK9OfaPyzeiOc44xdE_DMc52AsTAvrvkItE_8_RRTDD2NayEB8gjM9WdB4bazJIRfetX7WmCf0IwgbXmTWPCQBOXZHlkstzNNbrkWAckOzvd7vW8Jhz1i-MUP_-MhEeeG7Oa-x---jsg9fev7L_Tb8jSUVZw9PzKU3bp_N46eb-YY4nRMl1-V_4nN08wzzdiT_Zh6UAmAUAax3atWJodXFBKdjdXssgsvxqyREMLH3LDh1Gev81S2lkxUiLzEAVDsc-FtCfSjwk8KeOGaDcBFEC6cpLtCI8sDWcvmVqHMCzdCebPxWr4=")
+    API_ID = int(os.environ.get("APP_ID", "16715400"))
+    API_HASH = os.environ.get("API_HASH", "2de611a73f73b8da8b0bcee9f855c815")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5417392757:AAFt3O1iesTxa44Vi5xR7x9Qow_HjQfF1nI")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOI0Bu2WVe4sdTHIdQFwKICF8WqUKH6OdvpMs83T4L04tOzZbGjcedwEiQr8VnqbME7S5cofSuwPaMdWEneM8q20a4YHBwU4943tnpWpLsrsSXGGju11tvafYqNW23hMmZh8-OZ-ZAMYbChBlkrFfs1ZNTYwM318JnkWyBQxP11Kp0bpDjKVTyVUHPR73RquYz1fuBFBEJGKQcdhxCboIhzNH8v9suHR2Nrypt97tTsBe_P3dP31k9tNUanN_BNnENH8DqnSDtmNrlL4HTmtFfbUpXpjHidta98dbX4u_ak1yk5vF3WjFrYSvG1a3l-MYL_oivWvhm_upqTr22gTEaFC5aOg=")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "thehouse6bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Wo_69bot")
     SUPPORT = os.environ.get("SUPPORT", "ygabutkan")
     CHANNEL = os.environ.get("CHANNEL", "ygabutkan")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
